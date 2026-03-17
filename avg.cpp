5dbe47c66735acf2234eb97ae79e18d9eb5d6f18
shivanshsurjey <shivanshsurjey7@gmail.com>
2026-03-17 10:44:58 +0530

    Add files via upload

---
+++ avg.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+ int main(){
+     float n,m,o;
+     cout<<"Enetr marks of first subject"<<endl;
+      cin>>n;
+     cout<<"Enetr marks of second subject"<<endl;
+      cin>>m;
+     cout<<"Enetr marks of third subject"<<endl;
+      cin>>o;
+      
+      cout<<"Avg of threee subjects is: "<<(n+m+o)/3;
+      return 0;
+ }
