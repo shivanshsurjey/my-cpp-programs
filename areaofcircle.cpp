dde5f1adbcb2da9dcc67f7d99ecb6efc9d81a53c
shivanshsurjey <shivanshsurjey7@gmail.com>
2026-03-18 18:17:36 +0530

    find area of circle

---
+++ areaofcircle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    float r,p;
+    p=3.142;
+    cout<<"Enter the radius of circle : "<<endl;
+    cin>>r;
+    cout<<"Area of circle is : "<<( p*(r*r));
+
+    return 0;
+}
