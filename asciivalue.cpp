2711d9270cd1b408f67a96d6be6f487cd686b5cf
Shivansh Surjey <shivanshsurjey7@gmail.com>
2026-03-19 19:54:04 +0530

    find the ascii value of characters

---
+++ asciivalue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char x;
+    int value;
+    cout<<"Enter the character : ";
+    cin>>x;
+    value=x;
+    cout<<"ASCII value of given variable is : ";
+    
+    cout<<value;
+    return 0;
+    
+}
