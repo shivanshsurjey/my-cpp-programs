568d94bfa2da4a32456840187a4481cbe4e802cb
Shivansh Surjey <shivanshsurjey7@gmail.com>
2026-03-20 20:28:49 +0530

    find the output using operand of your choice

---
+++ userdefinedoperands.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of first number : "<<endl;
+    cin>>a;
+    cout<<"Enter the value of second number : "<<endl;
+    cin>>b;
+    cout<<"Sum of two number is : "<<endl<<(a+b)<<endl;
+    cout<<"subtraction of two number is : "<<endl<<(a-b)<<endl;
+    cout<<"multiplication of two number is : "<<endl<<(a*b)<<endl; 
+    cout<<"division of two number is : "<<endl<<(a/b)<<endl;
+    cout<<"Modulus of two number is : "<<endl<<(a%b)<<endl;
+    
+    return 0;
+    
+    
+}
