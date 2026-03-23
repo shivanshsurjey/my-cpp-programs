5748727ac9e50e4861b8d743ad8bf18770240910
Shivansh Surjey <shivanshsurjey7@gmail.com>
2026-03-23 19:49:27 +0530

    find the maximum of two numbers

---
+++ findmaximum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a,b;
+   cout<<"Enter the value of frst number :"<<endl;
+   cin>>a;
+   cout<<"Enter the value of second number :"<<endl;
+   cin>>b;
+   if(a>b){
+       cout<<"First is greater than second";
+   }else{cout<<"Second is greater than first";
+   }
+    
+    return 0;
+}
