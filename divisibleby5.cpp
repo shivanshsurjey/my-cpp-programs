e2fc4c72ab8e545e2f8e4decfc53334a56b3128f
Shivansh Surjey <shivanshsurjey7@gmail.com>
2026-03-24 21:04:47 +0530

    find if num is divisible by 5

---
+++ divisibleby5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n%5==0){
+        cout<<"it is divisible by 5";
+    }else{
+        cout<<"it is not divisible by 5";
+    }return 0;
+}
