080f2f606d7ec9910dbb1a2da48ee3d8039b222c
Shivansh Surjey <shivanshsurjey7@gmail.com>
2026-03-24 21:09:20 +0530

    find the number is even or odd

---
+++ evenodd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    if (num%2==0){
+        cout<<"THE NUMBER IS EVEN";
+    }
+    else{
+        cout<<"THE NUMBER IS ODD";
+    }
+    return 0;
+    
+}
